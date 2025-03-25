d80e6a5ba576d0c97eb465d8baa2c63ce38d1d95
SudhangsuShekharBairagi <bairagisudhangsu02@gmail.com>
2025-03-25 20:45:25 +0530

    modify data.php

--- php/data.php
+++ php/data.php
@@ -5,22 +5,45 @@
 
     while ($row = mysqli_fetch_assoc($query)) {
 
-        $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']}
+        $sql2 = "SELECT messages.msg_id, messages.msg, messages.outgoing_msg_id, messages.incoming_msg_id, 
+                       media.media_type, media.media_path FROM messages LEFT JOIN media ON messages.msg_id = media.msg_id 
+                       WHERE (incoming_msg_id = {$row['unique_id']}
                    OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id} 
                     OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
             $query2 = mysqli_query($conn, $sql2);
             $row2 = mysqli_fetch_assoc($query2);
-            (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="No message available";
-            (strlen($result) > 12) ? $msg =  substr($result, 0, 12) . '...' : $msg = $result;
 
+
+          
+    
+
+            // (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="No message available";
+            // (mysqli_num_rows($query2)>0) ? $result = $row2['media_type']: "hh";
+            global $msg;
+            if(mysqli_num_rows($query2) > 0){
+                if($row2['msg'] != null){
+                    $result = $row2['msg'];
+                    (strlen($result) > 12) ? $msg =  substr($result, 0, 12) . '...' : $msg = $result;
+                }
+                else{
+                    $result = $row2['media_path'];
+                    (strlen($result) > 12) ? $msg = '...'. substr($result, -6) : $msg = $result;
+                }
+            }else{
+                $msg ="No message available";
+            }
+            
+      
             if(isset($row2['outgoing_msg_id'])){
                 ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
             }else{
                 $you = "";
             }
+
             ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
             ($outgoing_id == $row['unique_id']) ? $hid_me = "hide" : $hid_me = "";
-    
+
+         
         $user_id = $row['unique_id'];
         
         // Check if there is a pending message request involving the logged-in user
